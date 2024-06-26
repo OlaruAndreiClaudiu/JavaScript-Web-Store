685457bbae37577d399c0e6c29714b7c41a94348
Olaru Andrei Claudiu <olaru.andrei.claudiu@gmail.com>
2024-06-26 11:42:55 +0300

    Commit 11

--- webstore.js
+++ webstore.js
@@ -4,24 +4,28 @@ const product = [
         image: './assets/img/image1.jpg',
         title: 'Push Up Bars',
         price: 50,
+        number: 10,
     },
     {
         id: 1,
         image: './assets/img/image2.jpg',
         title: 'Dumbbell',
         price: 150,
+        number: 20,
     },
     {
         id: 2,
         image: './assets/img/image3.jpg',
         title: 'Pull Up Bar',
         price: 250,
+        number: 30,
     },
     {
         id: 3,
         image: './assets/img/image4.jpg',
         title: 'Barbell',
         price: 350,
+        number: 40,
     }
 ];
 const products = [...new Set(product.map((item)=>
@@ -70,6 +74,9 @@ function displaycart() {
                 <div class='row-img'>
                 <img class='rowimg' src=${image}>
                 </div>
+                <button onclick="totalClick(1)" style='width: 10px;'>+</button>
+                <span id="totalClicks">1</span>
+                <button onclick="totalClick(-1)" style='width: 10px;'>-</button>
                 <p style='font-size: 12px;'>${title}</p>
                 <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
                 "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
@@ -101,3 +108,17 @@ function checkout() {
 }
 document.getElementById("checkout-btn").addEventListener("click", checkout);
 renderProducts();
+function totalClick(click) {
+    let total=0;
+    const totalClicks = document.getElementById('totalClicks');
+    const sumvalue = parseInt(totalClicks.innerText) + click;
+    totalClicks.innerText = sumvalue;
+    if(sumvalue > 1) {
+        total = total + price;
+        document.getElementById("total").innerHTML = "$ "+total+".00";
+        return;
+    }
+    if(sumvalue <= 1) {
+        totalClicks.innerText = 1;
+    }
+}
