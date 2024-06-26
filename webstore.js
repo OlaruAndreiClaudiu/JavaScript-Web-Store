829f0d4b4492ac38bfeb9133345adfd6bd96a1ba
Olaru Andrei Claudiu <olaru.andrei.claudiu@gmail.com>
2024-06-26 08:09:35 +0300

    Commit 8

--- webstore.js
+++ webstore.js
@@ -43,9 +43,7 @@ document.getElementById('root').innerHTML = products.map((item)=>
         </div>`
     )
 }).join('');
-
 var cart =[];
-
 function addtocart(a) {
     cart.push({...products[a]});
     displaycart();
@@ -54,13 +52,13 @@ function delElement(a) {
     cart.splice(a, 1);
     displaycart();
 }
-
 function displaycart() {
-    let j = 0, total=0;
+    let j = 0, total=0, quantity=0;
     document.getElementById("count").innerHTML=cart.length;
     if(cart.length == 0) {
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
         document.getElementById("total").innerHTML = "$ "+0+".00";
+        document.getElementById("quantity").innerHTML =""+0+"";
     }
     else {
         document.getElementById("cartItem").innerHTML = cart.map((items)=>
@@ -68,11 +66,15 @@ function displaycart() {
             var {image, title, price} = items;
             total = total + price;
             document.getElementById("total").innerHTML = "$ "+total+".00";
+            document.getElementById("quantity").innerHTML =""+quantity+"";
             return(
                 `<div class='cart-item'>
                 <div class='row-img'>
                 <img class='rowimg' src=${image}>
                 </div>
+                <button class="cart-qty-plus" type="button" value="+">+</button>
+                <p>${quantity}</p>
+                <button class="cart-qty-minus" type="button" value="-">-</button>
                 <p style='font-size: 12px;'>${title}</p>
                 <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
                 "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
@@ -80,6 +82,15 @@ function displaycart() {
         }).join('');
     } 
 }
+function clear() {
+    document.getElementById('cartItem').innerHTML = "Your cart is empty";
+    document.getElementById("total").innerHTML = "$ "+0+".00";
+    document.getElementById("count").innerHTML = 0;
+    cart.length = 0;
+    renderCart();
+}
+document.getElementById("clear-btn").addEventListener("click", clear);
+renderProducts();
 function checkout() {
     if(cart.length == 0) {
         alert(`Your cart is empty!`);
