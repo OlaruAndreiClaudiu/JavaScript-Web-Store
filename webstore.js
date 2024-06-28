d2fff7bda80e81c8c4979521c9eac45eb89cb31b
Olaru Andrei Claudiu <olaru.andrei.claudiu@gmail.com>
2024-06-28 03:29:11 +0300

    Commit 12

--- webstore.js
+++ webstore.js
@@ -4,28 +4,32 @@ const product = [
         image: './assets/img/image1.jpg',
         title: 'Push Up Bars',
         price: 50,
-        number: 10,
+        stock: 10,
+        description: 'Bratele pentru flotari sunt perfecte pentru cei ce doresc sa lucreze intens muschii bratelor, si umerilor, si spatelui, si nu numai. Pot fi folositi de orice persoana, oferind mai mult confort si stabilitate exercitiilor. Sunt fabricate din materiale de calitate, ceea ce le face sa fie durabile. Manerele sunt buretate pentru a proteja mainile, iar piciorusele invelite in cauciuc nu permit bratelor sa alunece in timpul exercitiilor.',
     },
     {
         id: 1,
         image: './assets/img/image2.jpg',
         title: 'Dumbbell',
         price: 150,
-        number: 20,
+        stock: 20,
+        description: 'Forma sa hexagonala o impiedica sa se rostogoleasca (6 laturi). Este acoperita cu un strat foarte rezistent si durabil de cauciuc care o protejează de socuri si minimizeaza zgomotul. Maner ergonomic cromat cu zona antiderapanta pentru o prindere perfecta.',
     },
     {
         id: 2,
         image: './assets/img/image3.jpg',
         title: 'Pull Up Bar',
         price: 250,
-        number: 30,
+        stock: 30,
+        description: 'O bara de tractiuni este dispozitivul perfect pentru exercitii. Il poti atasa cu usurinta pe perete, facandu-l stabil si sigur. Este potrivit atat pentru incepatori, cat si pentru sportivii experimentati. Bara poate rezista la o sarcina de pana la 90 kg, ceea ce o face solida si fiabila.',
     },
     {
         id: 3,
         image: './assets/img/image4.jpg',
         title: 'Barbell',
         price: 350,
-        number: 40,
+        stock: 40,
+        description: 'Bara este conceputa in principal pentru Body Pump, un antrenament de fitness bazat pe forta si rezistenta care intareste muschii, imbunatateste conditia fizica si ajuta la arderea grasimilor corporale. Setul Body Pump va permite sa antrenati diferite grupe de muschi fara a fi nevoie sa cumparati alte accesorii de fitness. Faceti ghemuituri, fandari, tractiuni, flotari, flotari pe banca si alte exercitii cu greutati pentru a va asigura rezultate mai rapide sub forma unor muschi dezvoltati si a unei pierderi de grasime. Exercitiu cu echipamente de cea mai buna calitate!',
     }
 ];
 const products = [...new Set(product.map((item)=>
@@ -33,15 +37,24 @@ const products = [...new Set(product.map((item)=>
 let i=0;
 document.getElementById('root').innerHTML = products.map((item)=>
 {
-    var {image, title, price} = item;
+    var {image, title, price, description} = item;
     return(
         `<div class='box'>
-            <div class='img-box'>
-                <img class='image' src=${image}></img>
-            </div>
+        <div class='img-box'>
+        <img class='image' src=${image}></img>
+        </div>
         <div class='bottom'>
         <p>${title}</p>
-        <h2>$ ${price}.00</h2>`+
+        <h2>$ ${price}.00</h2>
+        <div class="popup" id="popup-1">
+        <div class="overlay"></div>
+        <div class="content">
+        <div class="close-btn" onclick="togglePopup()">&times;</div>
+        <h1>Description</h1>
+        <p>${description}</p>
+        </div>
+        <button onclick="togglePopup()">Details</button>
+        </div>`+
         "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
         `</div>
         </div>`
@@ -66,7 +79,7 @@ function displaycart() {
     else {
         document.getElementById("cartItem").innerHTML = cart.map((items)=>
         {
-            var {image, title, price} = items;
+            var {image, title, price, id} = items;
             total = total + price;
             document.getElementById("total").innerHTML = "$ "+total+".00";
             return(
@@ -74,6 +87,7 @@ function displaycart() {
                 <div class='row-img'>
                 <img class='rowimg' src=${image}>
                 </div>
+                <p>${id}</p>
                 <button onclick="totalClick(1)" style='width: 10px;'>+</button>
                 <span id="totalClicks">1</span>
                 <button onclick="totalClick(-1)" style='width: 10px;'>-</button>
@@ -109,16 +123,16 @@ function checkout() {
 document.getElementById("checkout-btn").addEventListener("click", checkout);
 renderProducts();
 function totalClick(click) {
-    let total=0;
     const totalClicks = document.getElementById('totalClicks');
     const sumvalue = parseInt(totalClicks.innerText) + click;
     totalClicks.innerText = sumvalue;
-    if(sumvalue > 1) {
-        total = total + price;
-        document.getElementById("total").innerHTML = "$ "+total+".00";
-        return;
-    }
     if(sumvalue <= 1) {
         totalClicks.innerText = 1;
     }
+    if(sumvalue > stock) {
+        totalClicks.innerText = 1;
+    }
+}
+function togglePopup() {
+    document.getElementById("popup-1").classList.toggle("active");
 }
